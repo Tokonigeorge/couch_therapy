e601cae3271dcccdf4916f5b583663feb1d05e63
Bukola <bukolaebikefe@yahoo.com>
2021-09-15 22:38:18 +0100

    Fixes #15 , #11 , #4 , #3 , #2

--- src/layouts/GetstartedPage.jsx
+++ src/layouts/GetstartedPage.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../components/Button";
 import InputWrapper from "../components/inputWrapper";
+import logo from "../assests/logo.svg";
+import Login from "../components/Login";
 
 const GetstartedPage = () => {
+  const [checked, setChecked] = useState(false);
   return (
-    <div>
+    <div className="h-screen flex flex-col items-center px-10 pt-4">
+      <img src={logo} alt="Logo" />
+      <p className="text-4xl font-bold text-center mt-4">Create your Account</p>
       <InputWrapper
         type="text"
         label="First Name"
@@ -17,6 +23,27 @@ const GetstartedPage = () => {
         placeholder="What's your family name"
       />
       <InputWrapper label="Phone Number" />
+      <label className="text-sm text-gray-400 inline-flex items-center mt-4">
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+          className="mr-1.5"
+        />
+        <span>
+          {" "}
+          By creating an account you agree to phastmoney's{" "}
+          <a href="/" className="underline">
+            Privacy
+          </a>{" "}
+          and{" "}
+          <a href="/" className="underline">
+            Terms of use.
+          </a>
+        </span>
+      </label>
+      <Button prop="bg-black mt-4 mb-4" text="Get Started" />
+      <Login />
     </div>
   );
 };
