2c06a2d1f37b27b3f0da31030725382e84cb1014
Bukola <bukolaebikefe@yahoo.com>
2021-09-19 01:11:33 +0100

    [CampPage] Craeted navbar, Fixes #32

--- src/components/NavTop.jsx
+++ src/components/NavTop.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import profileIcon from "../assests/profileIcon.svg";
 
-const NavTop = ({ userName, dailyMessage }) => {
+const NavTop = ({ userName, dailyMessage, header }) => {
   return (
     <div className="flex flex-row justify-between items-center px-6">
       <div>
         <p className="text-xl font-semibold">
-          Hi there <span className="font-bold">{userName}!</span>
+          {header}{" "}
+          <span className="font-bold">{userName && userName + "!"} </span>
         </p>
         <p className="text-gray-400 ">{dailyMessage}</p>
       </div>
